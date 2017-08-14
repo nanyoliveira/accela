49e987c9c02148b620a4f68cad27110faa109236
Ariane <ariane@nativoo.com>
2017-08-14 18:44:57 +0100

    Initial Commit

---
+++ Accela/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Accela
+//
+//  Created by Matt Ariane Clarke on 14/08/2017.
+//  Copyright © 2017 MN MobileDevelopers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Accela/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Accela
+//
+//  Created by Matt Ariane Clarke on 14/08/2017.
+//  Copyright © 2017 MN MobileDevelopers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
